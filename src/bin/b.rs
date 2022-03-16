7b4c6798e38075581d7bc7939cdccacb129c5145
Kenbayashi <kenbayashi.gaming.00@gmail.com>
2022-03-16 13:49:17 +0900

    b

--- src/bin/b.rs
+++ src/bin/b.rs
@@ -1,3 +1,57 @@
+use proconio::input;
+use proconio::marker::Chars;
+
 fn main() {
-    unimplemented!();
+    input! {
+        s: Chars,
+    };
+
+    let mut accept = true;
+
+    let mut state = 0u64;
+
+    for c in s.into_iter() {
+        match state {
+            0 => {
+                state = if c == 'x' {1} else {2};
+            },
+
+            // 1文字目がxだったとき
+            1 => {
+                state = if c == 'o' {2} else {4};
+            },
+
+            // oだったとき
+            2 => {
+                if c == 'x' {
+                    state = 3;
+                } else {
+                    accept = false;
+                    break;
+                }
+            },
+
+            3 => {
+                if c == 'x' {
+                    state = 4;
+                } else {
+                    accept = false;
+                    break;
+                }
+            },
+
+            4 => {
+                if c == 'o' {
+                    state = 2;
+                } else {
+                    accept = false;
+                    break;
+                }
+            },
+
+            _ => (),
+        }
+    }
+
+    println!("{}", if accept {"Yes"} else {"No"});
 }
