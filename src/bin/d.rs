3916337dee56bc5e029328294fa46879f8fdd531
Kenbayashi <kenbayashi.gaming.00@gmail.com>
2022-03-17 13:09:36 +0900

    d

--- src/bin/d.rs
+++ src/bin/d.rs
@@ -1,3 +1,24 @@
 fn main() {
-    unimplemented!();
+    proconio::input! {
+        n: i64,
+        d: i64,
+        mut walls: [(i64, i64); n],
+    };
+
+    walls.sort_by_key(|&(_, r)| r);
+
+    let mut count = 0;
+
+    while 0 != walls.len() {
+        let &(_, right_edge) = walls.get(0).unwrap();
+        let punch = right_edge + d - 1;
+
+        walls = walls.into_iter()
+                     .filter(|&(left_edge, _)| punch < left_edge)
+                     .collect::<Vec<(i64, i64)>>();
+
+        count += 1;
+    }
+
+    println!("{}", count);
 }
